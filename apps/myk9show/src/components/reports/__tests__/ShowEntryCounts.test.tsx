c04784cb5c64cd19d154710ab8838a2f7f53c424
rbeezley <138222684+rbeezley@users.noreply.github.com>
2026-04-11 10:50:45 -0500

    test(reports): use custom render in ShowEntryCounts test

--- apps/myk9show/src/components/reports/__tests__/ShowEntryCounts.test.tsx
+++ apps/myk9show/src/components/reports/__tests__/ShowEntryCounts.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '@/test/utils/testUtils';
 import { ShowEntryCounts } from '../ShowEntryCounts';
 import type { ReportProps } from '@/lib/reports/types';
 

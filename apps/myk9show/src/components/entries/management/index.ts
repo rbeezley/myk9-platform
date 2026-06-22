7f2867f9a9dd461a41928a1fc2ef6a8854d2d28e
Richard Beezley <beezley@cox.net>
2026-06-22 10:40:00 -0500

    WIP(entries): Phase C - Exceptions tab hosting move-ups/pulls sub-switch

--- apps/myk9show/src/components/entries/management/index.ts
+++ apps/myk9show/src/components/entries/management/index.ts
@@ -15,3 +15,4 @@ export { TrialClassFilters } from './TrialClassFilters';
 export { TrialRosterView } from './TrialRosterView';
 export { ScoringModeWrapper } from './ScoringModeWrapper';
 export { RegistrationView } from './RegistrationView';
+export { ExceptionsView } from './ExceptionsView';

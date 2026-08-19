65f9a31e27d42d3629845dc5eca123ca9ca8a26e
Richard Beezley <beezley@cox.net>
2026-08-18 21:53:49 -0500

    test(storage): register image RLS regression

--- scripts/qa/run-behavioral-sql-tests.test.ts
+++ scripts/qa/run-behavioral-sql-tests.test.ts
@@ -40,6 +40,7 @@ const launchCriticalSqlTests = [
   'office_admin_rls_test.sql',
   'entry_status_history_rls_test.sql',
   'entry_views_soft_delete_test.sql',
+  'image_storage_upsert_rls_test.sql',
   'myk9_114_entry_access_context_test.sql',
   'myk9_169_role_boundaries_test.sql',
   'paperwork_prints_rls_test.sql',

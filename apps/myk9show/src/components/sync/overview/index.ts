b58ba2df312736e4218c14f3638eccb5bf9160a6
Richard Beezley <beezley@cox.net>
2026-09-02 19:06:29 -0500

    chore: remove confirmed dead component families

--- apps/myk9show/src/components/sync/overview/index.ts
+++
@@ -1,5 +0,0 @@
-export { ComprehensiveSyncDashboard } from './ComprehensiveSyncDashboard';
-export { SyncOverviewWidget } from './SyncOverviewWidget';
-export { GlobalSyncControls } from './GlobalSyncControls';
-export { SyncNotificationCenter } from './SyncNotificationCenter';
-export { SyncAnalyticsDashboard } from './SyncAnalyticsDashboard';
